34bac52994db04b3f790d8f74c37f6e60cdc9b76
Lunny Xiao <xiaolunwen@gmail.com>
2022-03-21 13:22:37 +0800

    Update docs and remove unnecessary variables

--- services/pull/merge.go
+++ services/pull/merge.go
@@ -32,7 +32,6 @@ import (
 	asymkey_service "code.gitea.io/gitea/services/asymkey"
 	issue_service "code.gitea.io/gitea/services/issue"
 
-	"gitea.com/lunny/size"
 	"github.com/unknwon/com"
 )
 
@@ -64,7 +63,7 @@ func GetDefaultMergeMessage(baseGitRepo *git.Repository, pr *models.PullRequest,
 			log.Error("GetBranchCommit: %v", err)
 			return ""
 		}
-		templateContent, err := commit.GetFileContent(templateFilepath, int(10*size.K))
+		templateContent, err := commit.GetFileContent(templateFilepath, 10*1024)
 		if err != nil {
 			log.Error("GetFileContent: %v", err)
 			return ""
@@ -82,7 +81,6 @@ func GetDefaultMergeMessage(baseGitRepo *git.Repository, pr *models.PullRequest,
 			"PullRequestPosterName":  pr.Issue.Poster.Name,
 			"PullRequestIndex":       strconv.FormatInt(pr.Index, 10),
 			"PullRequestReference":   fmt.Sprintf("%s%d", issueReference, pr.Index),
-			"IssueReferenceChar":     issueReference,
 		}
 		refs, err := pr.ResolveCrossReferences()
 		if err == nil {
